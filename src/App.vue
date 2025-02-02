d20b64d06cb2c4df243136ae850bafab81389ad0
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2025-02-02 19:52:51 -0300

    add Example3Loop component and update App.vue to include it

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
 import Example1 from "./components/Example1Interpolation.vue";
 
 import Example2 from "./components/Example2Conditional.vue";
+
+// Vector
+import Example3 from "./components/Example3Loop.vue";
 </script>
 
 <!-- HTML -->
@@ -16,5 +19,10 @@ import Example2 from "./components/Example2Conditional.vue";
   <div>
     <h1>Example 2 - Conditional</h1>
     <Example2 />
+  </div>
+
+  <div>
+    <h1>Example 3 - Loop</h1>
+    <Example3 />
   </div>
 </template>
---
+++ src/components/Example3Loop.vue
@@ -0,0 +1,12 @@
+<!-- Script -->
+<script setup>
+// Vector
+let names = ["Ana", "Maria", "José", "Carlos", "Pedro"];
+</script>
+
+<!-- HTML -->
+<template>
+  <ul>
+    <li v-for="name in names" :key="name">{{ name }}</li>
+  </ul>
+</template>
