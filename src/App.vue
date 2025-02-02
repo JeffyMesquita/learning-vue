e58c46954d78e847571e966e5880ad761c2b9bed
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2025-02-02 19:28:54 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<!-- Script -->
+<script setup>
+// Variables
+let nome = "Jeffy Mesquita";
+let idade = 37;
+let numero1 = 10;
+let numero2 = 20;
+</script>
+
+<!-- HTML -->
+<template>
+  <p>
+    {{ "Hello, " + nome + " você tem " + idade + " anos." }}
+  </p>
+  <p>
+    {{ numero1 + numero2 }}
+  </p>
+</template>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+// Módulo createApp
+import { createApp } from "vue";
+
+// Importa o arquivo App.vue
+import App from "./App.vue";
+
+// Monta o App.vue no elemento com id #app (Renderiza o App.vue no elemento com id #app)
+createApp(App).mount("#app");
