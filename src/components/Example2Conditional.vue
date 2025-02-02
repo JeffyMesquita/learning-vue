e20dcc3cf31e0d1397ba7ce4089bf20527ff184e
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2025-02-02 19:46:59 -0300

    update Example2Conditional component to modify visibility logic and add average grading display

--- src/components/Example2Conditional.vue
+++ src/components/Example2Conditional.vue
@@ -1,10 +1,15 @@
 <!-- SCRIPT -->
 <script setup>
 // Variables
-let show = true;
+let show = false;
+let average = 4;
 </script>
 
 <!-- HTML -->
 <template>
   <h1 v-if="show">Hello, Vue 3!</h1>
+
+  <p v-if="average >= 7" style="color: green">Aprovado(a)</p>
+  <p v-else-if="average >= 5 && average < 7" style="color: orange">Em exame</p>
+  <p v-else style="color: red">Reprovado(a)</p>
 </template>
